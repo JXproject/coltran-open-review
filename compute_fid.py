cecbbc49fc39f5de9fb8bbd9ae99a0aa2b6a3627
Pranav Barot <pranavbarot@rocketmail.com>
2021-12-04 15:33:12 -0500

    Add files via upload

--- compute_fid.py
+++ compute_fid.py
@@ -21,6 +21,7 @@ def scale_images(images, new_shape):
 		images_list.append(new_image)
 	return np.asarray(images_list)
  
+
 # calculate frechet inception distance
 def calculate_fid(model, images1, images2):
 	# calculate activations
@@ -41,6 +42,8 @@ def calculate_fid(model, images1, images2):
 	return fid
 
 def predict(images1, images2):
+	# images1 = np.array(images1).astype('float32')
+	# images2 = np.array(images2).astype('float32')
 	images1 = images1.astype('float32')
 	images2 = images2.astype('float32')
 	# resize images
@@ -64,26 +67,45 @@ if __name__ == "__main__":
 	b2 = 'batch2/*'
 	b3 = 'batch3/*'
 
+	num_imgs = 10
 	model = InceptionV3(include_top=False, pooling='avg', input_shape=(299,299,3))
 
+	orig = [imread(glob(original)[idx]) for idx in range(0,num_imgs)]
+	b1 = [imread(glob(b1)[idx]) for idx in range(0,num_imgs)]
+	b2 = [imread(glob(b2)[idx]) for idx in range(0,num_imgs)]
+	b3 = [imread(glob(b3)[idx]) for idx in range(0,num_imgs)]
 
-	if len(sys.argv) >= 1:
+	all_images = [original, b1, b2, b3]
+
+	data = np.zeros((1, 4))
+
+	f1 = predict(orig, b1)
+	f2 = predict(orig, b2)
+	f3 = predict(orig, b3)
+
+
+	# print(sorted(glob(original)), sorted(glob(b1)))
+	# for i, idx in enumerate(range(0,len(glob(original)))):
+	# 	orig = imread(sorted(glob(original))[idx], plugin='matplotlib')
+	# 	i1 = imread(sorted(glob(b1))[idx], plugin='matplotlib')
+	# 	i2 = imread(sorted(glob(b2))[idx], plugin='matplotlib')
+	# 	i3 = imread(sorted(glob(b3))[idx], plugin='matplotlib')
 		
-		all_images = [original, b1, b2, b3]
+	f1 = predict(orig, i1)
+	print('OUTPUT 1: %.3f' %f1)
 
-		idx = int(sys.argv[1])
-		orig = imread(glob(original)[idx], plugin='matplotlib')
-		i1 = imread(glob(b1)[idx], plugin='matplotlib')
-		i2 = imread(glob(b2)[idx], plugin='matplotlib')
-		i3 = imread(glob(b3)[idx], plugin='matplotlib')
-		
-		f1 = predict(orig, i1)
-		print('OUTPUT 1: %.3f' %f1)
+	f2 = predict(orig, i2)
+	print('OUTPUT 2: %.3f' %f2)
 
-		f2 = predict(orig, i2)
-		print('OUTPUT 2: %.3f' %f2)
+	f3 = predict(orig, i3)
+	print('OUTPUT 3: %.3f' %f3)
+	# 	# convert integer to floating point values
 
-		f3 = predict(orig, i3)
-		print('OUTPUT 3: %.3f' %f3)
-		# convert integer to floating point values
+	data[0,0] = idx
+	data[0,1] = f1
+	data[0,2] = f2
+	data[0,3] = f3
+
+	np.save('fid_scores.npy', data)
+
 		
